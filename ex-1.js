df6776a55621e32e108ef131fe0b857308250ae7
Thanasak <ma.thanasak@gmail.com>
2024-05-09 13:46:19 +0700

    feat: Resolve Conflict

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,5 @@
 let accountBalance = 400;
 let depositAmountFromUser = "10";
-
-accountBalance = accountBalance + depositAmountFromUser;
-
+accountBalance = accountBalance + Number(depositAmountFromUser);
 let accountBalanceMessage = "Your account balance is " + accountBalance;
-
 console.log(accountBalanceMessage);
